13015059c686c028bbecd0608ca3446e65cee67b
Gianluca Girardi <gl.girardi1@gmail.com>
2025-06-22 14:36:32 -0400

    Configured rntl cid-cd and prettier

---
+++ httc-frontend/components/Hello.tsx
@@ -0,0 +1,11 @@
+// components/Hello.tsx
+import React from "react";
+import { Text, View } from "react-native";
+
+export const Hello = () => {
+  return (
+    <View>
+      <Text>Hello, Luca!</Text>
+    </View>
+  );
+};
